a9ef4dcb54e721f928ee8f9106056f9bc7f25aa4
sjcswank <sjcswank@gmail.com>
2019-07-12 19:36:22 -0500

    Add API call to About

---
+++ src/app/_services/about.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import { map} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AboutService {
+
+  private username: string;
+  private clientid = '4193694183d06625d9af';
+  private clientsecret = 'f59b79db7482af00c2231633b5c81219021c49fd';
+
+  constructor(private http:Http) {
+	  console.log("service is ready");
+	  this.username = 'sjcswank';
+  }
+
+  getUserData(){
+  	return this.http.get("https://api.github.com/users/" + this.username + '?client_id=' + this.clientid + '&client_secret' + this.clientsecret).pipe(map(res => res.json()));
+  }
+
+}
